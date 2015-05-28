5464f087fe916ab8f04b30e8c569a474201c13d1
Carl Christensen <exe0cdc@gmail.com>
2015-05-28 14:42:32 +0200

    Added shortcuts to Symca, ThermoKin, RateChar and ModelGraph to the root namespace.

--- psctb/__init__.py
+++ psctb/__init__.py
@@ -2,3 +2,7 @@ from . import modeltools
 from . import latextools
 from . import utils
 from . import analyse
+from .analyse import Symca
+from .analyse import RateChar
+from .analyse import ThermoKin
+from .utils.model_graph import ModelGraph
