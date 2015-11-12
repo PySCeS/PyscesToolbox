dbd015086af1411e90a8196b8993e73a6ed2b93b
Carl Christensen <exe0cdc@gmail.com>
2015-11-12 16:42:44 +0200

    Add some symca docstring

--- psctb/analyse/_symca/_symca.py
+++ psctb/analyse/_symca/_symca.py
@@ -14,6 +14,23 @@ all = ['Symca']
 
 
 class Symca(object):
+    """
+    A class that performs Symbolic Metabolic Control Analysis.
+
+    This class takes pysces model as an input and performs symbolic inversion
+    of the ``E matrix`` using ``Sympy`` by calculating the determinant and
+    adjoint matrices of this ``E matrix``.
+
+    Parameters
+    ----------
+    mod : PysMod
+        The pysces model on which to perform symbolic control analysis.
+    auto_load : boolean
+        If true
+
+    Returns
+    ------
+    """
     def __init__(self, mod, auto_load=False, internal_fixed=False):
         super(Symca, self).__init__()
 
