8783846a11c2d05fcd09634a75953139a5cc2e26
Carl Christensen <exe0cdc@gmail.com>
2017-07-31 14:09:37 +0200

    Update setup.py, fix d3networkx installation

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+from os import path
 try:
   from jupyterpip import cmdclass
 except:
@@ -7,18 +8,25 @@ except:
   pip.main(['install', 'jupyter-pip'])
   cmdclass = importlib.import_module('jupyterpip').cmdclass
 
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.rst')) as f:
+    long_description = f.read()
+
 packages = find_packages()
 
 setup(
     name='PyscesToolbox',
-    version='0.8.6',
+    version='0.8.7',
     packages=packages,
     url='https://github.com/PySCeS/PyscesToolbox',
-    download_url='http://github.com/PySCeS/PyscesToolbox/archive/0.8.6.tar.gz',
+    download_url='http://github.com/PySCeS/PyscesToolbox/archive/0.8.7.tar.gz',
     license='BSD-3-Clause',
     author='Carl Christensen',
     author_email='carldc@sun.ac.za',
     description='A set of metabolic model analysis tools for PySCeS.',
+    long_description=long_description,
     install_requires=['IPython>=4.0.0',
                       'numpy',
                       'sympy',
@@ -29,6 +37,7 @@ setup(
                       'networkx',
                       'jupyter',
                       ],
+    package_data={'d3networkx_psctb': ['widget.js'],},
     keywords=['metabolism','metabolic control analysis','modelling'],
     classifiers=['Development Status :: 5 - Production/Stable',
                  'Intended Audience :: Science/Research',
