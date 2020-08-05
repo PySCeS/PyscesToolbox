cd3c2f1048e9a9996bd7830f1cb60da8fdb500a0
Carl Christensen <exe0cdc@gmail.com>
2020-08-05 21:35:24 +0200

    Fix saving of ModelGraph svg images.
    Somewhere a change occured that caused the svg code defining the
    image to be wrapped in some additional xml cruft. This fix simply
    cuts out the relavent svg from the original xml string and saves it
    instead of the whole xml document. Ideally this change should have
    occured in ForceDirectedGraph, however the svg_image attribute is empty
    until the save() method of ModelGraph is called. To fix this without
    digging in to the details too much, the fix was applied at the
    ModelGraph level.

--- psctb/utils/model_graph/_model_graph.py
+++ psctb/utils/model_graph/_model_graph.py
@@ -16,10 +16,19 @@ from d3networkx_psctb import EventfulGraph
 from ... import modeltools
 from ..misc import split_coefficient
 
-
 __author__ = 'carl'
 
 __all__ = ['ModelGraph']
+
+
+def extract_svg_from_xml(xml_string):
+    start_string = '<svg'
+    end_string = '</svg>'
+
+    start_index = xml_string.index(start_string)
+    end_index = xml_string.index(end_string) + len(end_string)
+
+    return xml_string[start_index:end_index]
 
 
 class ModelGraph(object):
@@ -54,8 +63,7 @@ class ModelGraph(object):
                     8: "rgb(213,62,79)", 9: "rgb(158,1,66)",
                     10: "rgb(158,1,66)"}
 
-    DUMMY_SINK_NAMES = ['dummy','sink']
-
+    DUMMY_SINK_NAMES = ['dummy', 'sink']
 
     def __init__(self, mod, pos_dic=None, analysis_method=None,
                  base_name=None):
@@ -111,7 +119,6 @@ class ModelGraph(object):
         for rx in self._model_map.reactions:
             self._pos_dic[rx.name] = ('none', 'none')
 
-
     # TODO Change d3networkx_psctb so that dumping json and dumping svg work in the same way
     def _pos_change_setup(self):
         def make_pos_dic(nodes_json):
@@ -131,13 +138,12 @@ class ModelGraph(object):
         self._force_directed_graph.on_trait_change(save_json, 'gnode_json')
 
     def _save_image_setup(self):
-        def save_image(sender, content):
+        def save_image(sender, content, *args):
             if 'button_click' in content:
                 if content['button_click'] == 'save_image':
                     self.save()
 
         self._force_directed_graph.on_msg(save_image)
-
 
     def _make_species_nodes(self):
         for species in self._model_map.species:
@@ -241,7 +247,6 @@ class ModelGraph(object):
                 del self._eventful_graph._adj[reaction][species.name]
                 self._ec_dict[elas] = (species.name, reaction)
 
-
     def remove_external_modifier_links(self):
         for species in self._model_map.species:
             for reaction in species.isModifierOf():
@@ -276,7 +281,6 @@ class ModelGraph(object):
             self.change_node_properties(reaction.name,
                                         ModelGraph.REACTION_NODE)
 
-
     def change_node_properties(self, node_name, prop_dic=None):
         if not prop_dic:
             prop_dic = {}
@@ -300,7 +304,6 @@ class ModelGraph(object):
         for node in self._model_map.reactions + self._model_map.species:
             self.change_node_properties(node.name,
                                         {'fixed': self._nodes_fixed})
-
 
     @property
     def straight_links(self):
@@ -340,16 +343,17 @@ class ModelGraph(object):
         -------
         None
         """
-
         svg = self._force_directed_graph.svg_image
+
+        svg = extract_svg_from_xml(svg)
 
         file_name = modeltools.get_file_path(working_dir=self._working_dir,
                                              internal_filename=self._base_name,
                                              fmt='svg',
                                              file_name=file_name)
+
         with open(file_name, 'w') as f:
             f.write(svg)
-
 
     def show(self, no_links=False, clear_top_box=True):
         if clear_top_box:
@@ -362,7 +366,6 @@ class ModelGraph(object):
         self.nodes_fixed = self._nodes_fixed
         if not no_links:
             self.draw_all_links()
-
 
     def highlight_cp(self, cp, show_dummy_sinks=False,
                      show_external_modifier_links=False):
@@ -407,7 +410,6 @@ class ModelGraph(object):
             self.remove_dummy_sinks()
         if not show_external_modifier_links:
             self.remove_external_modifier_links()
-
 
     def highlight_cc(self, cc, show_dummy_sinks=False,
                      show_external_modifier_links=False):
